032f4b511daaa45094b871c4e06065664d86a743
Nicks <Nicks@Betty>
2014-03-23 18:52:01 -0400

    Implémentation de wikitude dans CamActivity. Implémentation du bouton Quitter

--- com.Macrosoft.KaptAR/src/com/macrosoft/kaptar/CamActivity.java
+++ com.Macrosoft.KaptAR/src/com/macrosoft/kaptar/CamActivity.java
@@ -1,13 +1,166 @@
 package com.macrosoft.kaptar;
 
-import android.app.Activity;
+import java.io.IOException;
 
-public class CamActivity extends Activity
+import android.hardware.SensorManager;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.wikitude.architect.ArchitectView.ArchitectConfig;
+import com.wikitude.architect.ArchitectView.ArchitectUrlListener;
+import com.wikitude.architect.ArchitectView.SensorAccuracyChangeListener;
+
+public class CamActivity extends AbstractArchitectCamActivity
 {
+
+	/**
+	 * last time the calibration toast was shown, this avoids too many toast
+	 * shown when compass needs calibration
+	 */
+	private long	lastCalibrationToastShownTimeMillis	= System.currentTimeMillis();
+
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		// TODO Auto-generated method stub
+		super.onCreate( savedInstanceState );
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause()
+	{
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onPostCreate( Bundle savedInstanceState )
+	{
+		// TODO Auto-generated method stub
+		super.onPostCreate( savedInstanceState );
+
+		/*this.architectView.onPostCreate();
+		try
+		{
+			this.architectView.load( getIntent().getExtras().getString( Home.EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL ) );
+		}
+		catch( IOException e )
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+
+	@Override
+	protected void onPostResume()
+	{
+		// TODO Auto-generated method stub
+		super.onPostResume();
+	}
+
+	@Override
+	protected void onResume()
+	{
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
 
 	public CamActivity()
 	{
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public float getInitialCullingDistanceMeters()
+	{
+		// TODO Auto-generated method stub
+		return ArchitectViewHolderInterface.CULLING_DISTANCE_DEFAULT_METERS;
+	}
+
+	@Override
+	public String getActivityTitle()
+	{
+		// TODO Auto-generated method stub
+		return getIntent().getExtras().getString( Home.EXTRAS_KEY_ACTIVITY_TITLE_STRING );
+	}
+
+	@Override
+	public String getARchitectWorldPath()
+	{
+		// TODO Auto-generated method stub
+		return getIntent().getExtras().getString( Home.EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL );
+	}
+
+	@Override
+	public ArchitectUrlListener getUrlListener()
+	{
+		// TODO Auto-generated method stub
+		return new ArchitectUrlListener()
+		{
+
+			@Override
+			public boolean urlWasInvoked( String uriString )
+			{
+				// by default: no action applied when url was invoked
+				return false;
+			}
+		};
+	}
+
+	@Override
+	public int getContentViewId()
+	{
+		// TODO Auto-generated method stub
+		return R.layout.architectview_layout;
+	}
+
+	@Override
+	public String getWikitudeSDKLicenseKey()
+	{
+		// TODO Auto-generated method stub
+		return Home.Licence;
+	}
+
+	@Override
+	public int getArchitectViewId()
+	{
+		// TODO Auto-generated method stub
+		return R.id.architectView;
+	}
+
+	@Override
+	public ILocationProvider getLocationProvider( LocationListener locationListener )
+	{
+		return null;
+	}
+
+	@Override
+	public SensorAccuracyChangeListener getSensorAccuracyListener()
+	{
+		// TODO Auto-generated method stub
+		return new SensorAccuracyChangeListener()
+		{
+			@Override
+			public void onCompassAccuracyChanged( int accuracy )
+			{
+				/* UNRELIABLE = 0, LOW = 1, MEDIUM = 2, HIGH = 3 */
+				if( accuracy < SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM && CamActivity.this != null
+						&& CamActivity.this.isFinishing()
+						&& System.currentTimeMillis() - CamActivity.this.lastCalibrationToastShownTimeMillis > 5 * 1000 )
+				{
+					Toast.makeText( CamActivity.this, "low accuracy", Toast.LENGTH_LONG ).show();
+					CamActivity.this.lastCalibrationToastShownTimeMillis = System.currentTimeMillis();
+				}
+			}
+		};
 	}
 
 }
--- com.Macrosoft.KaptAR/src/com/macrosoft/kaptar/Home.java
+++ com.Macrosoft.KaptAR/src/com/macrosoft/kaptar/Home.java
@@ -1,6 +1,9 @@
 package com.macrosoft.kaptar;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,20 +14,39 @@ import android.widget.Toast;
 
 public class Home extends Activity implements OnClickListener
 {
-	Button btnScanNow = null;
-	Button btnConfiguration = null;
-	Button btnExit = null;
+	Button	btnScanNow			= null;
+	Button	btnConfiguration	= null;
+	Button	btnExit				= null;
 
+	/**
+	 * extras key for activity title, usually static and set in Manifest.xml
+	 */
+	protected static final String EXTRAS_KEY_ACTIVITY_TITLE_STRING = "activityTitle";
+	
+	/**
+	 * extras key for architect-url to load, usually already known upfront, can be relative folder to assets (myWorld.html --> assets/myWorld.html is loaded) or web-url ("http://myserver.com/myWorld.html"). Note that argument passing is only possible via web-url 
+	 */
+	protected static final String EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL = "activityArchitectWorldUrl";
+	
+	public static final String Licence = "abc";
+	
+
+	/**
+	 * last time the calibration toast was shown, this avoids too many toast shown when compass needs calibration
+	 */
+	private long lastCalibrationToastShownTimeMillis = System.currentTimeMillis();
+
+	
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_home );
-		
-		btnScanNow = (Button) findViewById( R.id.btnScanNow );
-		btnConfiguration = (Button) findViewById( R.id.btnSettings );
-		btnExit = (Button) findViewById( R.id.btnExit );
-		
+
+		btnScanNow = ( Button ) findViewById( R.id.btnScanNow );
+		btnConfiguration = ( Button ) findViewById( R.id.btnSettings );
+		btnExit = ( Button ) findViewById( R.id.btnExit );
+
 		btnScanNow.setOnClickListener( this );
 		btnConfiguration.setOnClickListener( this );
 		btnExit.setOnClickListener( this );
@@ -41,17 +63,18 @@ public class Home extends Activity implements OnClickListener
 	@Override
 	public void onClick( View arg0 )
 	{
-		if(arg0 == btnScanNow)
+		if( arg0 == btnScanNow )
 		{
-			Toast.makeText( this, "Scan!!!", Toast.LENGTH_SHORT ).show();
+			//Toast.makeText( this, CamActivity.class.getSimpleName() , Toast.LENGTH_SHORT ).show();
+			startArActivity();
 		}
-		else if(arg0 ==  btnScanNow)
+		else if( arg0 == btnConfiguration )
 		{
 			Toast.makeText( this, "Config!!!", Toast.LENGTH_SHORT ).show();
 		}
-		else if(arg0 == btnExit)
+		else if( arg0 == btnExit )
 		{
-			Toast.makeText( this, "Exit!!!", Toast.LENGTH_SHORT ).show();
+			leaveKaptAR();
 		}
 		else
 		{
@@ -59,4 +82,36 @@ public class Home extends Activity implements OnClickListener
 		}
 	}
 
+	public void startArActivity()
+	{
+		try
+		{
+			final Intent intent = new Intent(this, CamActivity.class);
+			intent.putExtra( EXTRAS_KEY_ACTIVITY_TITLE_STRING, "KaptAR World" );
+			intent.putExtra( EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL,
+					"KaptarWorld" + File.separator + "index.html" );
+
+			/* launch activity */
+			this.startActivity( intent );
+			
+
+		}
+		catch( Exception e )
+		{
+			/*
+			 * may never occur, as long as all SampleActivities exist and are
+			 * listed in manifest
+			 */
+			Toast.makeText( this, "CamActivity\nnot defined/accessible", Toast.LENGTH_SHORT ).show();
+		}
+		
+
+	}
+	
+	public void leaveKaptAR()
+	{
+		 Intent intent = new Intent(Intent.ACTION_MAIN);
+		 intent.addCategory(Intent.CATEGORY_HOME);
+		 startActivity(intent);
+	}
 }
